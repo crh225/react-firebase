6b6d2e40879fa55f23928347fc5ad1614c2b203a
CH <chris@chrishouse.us>
2018-08-04 18:24:45 -0500

    (fix): remove extra semi colon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Fabric} from 'office-ui-fabric-react/lib-commonjs/Fabric';
 import NavBar from './components/NavBar.js';
-import SidebarMenu from './components/SidebarMenu.js';;
+import SidebarMenu from './components/SidebarMenu.js';
 import Content from './components/Content.js';
 import Footer from './components/Footer.js';
 
