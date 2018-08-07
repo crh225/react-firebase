adcfcb717a909319d7c2fd28c54956213a22ecf4
CH <chris@chrishouse.us>
2018-08-06 19:14:38 -0500

    (refactor): moving components into barrels

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import {Fabric} from 'office-ui-fabric-react/lib-commonjs/Fabric';
-import NavBar from './components/NavBar.js';
-import SidebarMenu from './components/SidebarMenu.js';
-import Content from './components/Content.js';
-import Footer from './components/Footer.js';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { Fabric } from 'office-ui-fabric-react/lib-commonjs/Fabric';
+import { SidebarMenu, Footer, Content, NavBar } from './components';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Fabric className="App">       
+        <Fabric className="App">
           <div className="header">
             <NavBar />
-          </div>        
+          </div>
           <div className="body">
             <div className="content">
               <Content />
             </div>
             <div className="sidebar">
               <SidebarMenu />
-            </div>      
+            </div>
           </div>
           <div className="footer">
             <Footer />
-          </div>       
+          </div>
         </Fabric>
       </Router>
     );
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SidebarMenu } from "./SidebarMenu";
+export { default as Content } from "./Content";
+export { default as Footer } from "./Footer";
+export { default as NavBar } from "./NavBar";
